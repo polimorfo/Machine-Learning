f2fc84c7421782e2e1424ddf06cdcc66257082b7
laBolaDeCristal <jozze_01@yahoo.com>
2018-05-24 18:55:41 +0200

    Create forward-backguard API.py

---
+++ forward-backguard API.py
@@ -0,0 +1,13 @@
+ class ComputationalGraph(object):
+ 
+ def forward (inputs):
+ # 1 pass inputs to input gates
+ # forward the computational graph
+ for gate in self.graph.nodes_topologically_sorted():
+ gate.forward()
+ return loss # the final gate in the graps output the loss
+ 
+ def backguard():
+ for gate in reversed (self.graph.nodes_topologically_sorted()):
+ gate.backguard() # little piece of backp`rop(chain rule applied)
+ return input_gradients
