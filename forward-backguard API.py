6e609e685f55284a6581c441f9a3a8268b90fd77
laBolaDeCristal <jozze_01@yahoo.com>
2018-05-24 18:57:06 +0200

    computational graph pseudo code
    feeding and backprop

--- forward-backguard API.py
+++ forward-backguard API.py
@@ -3,11 +3,11 @@
  def forward (inputs):
  # 1 pass inputs to input gates
  # forward the computational graph
- for gate in self.graph.nodes_topologically_sorted():
- gate.forward()
+    for gate in self.graph.nodes_topologically_sorted():
+    gate.forward()
  return loss # the final gate in the graps output the loss
  
  def backguard():
- for gate in reversed (self.graph.nodes_topologically_sorted()):
- gate.backguard() # little piece of backp`rop(chain rule applied)
+     for gate in reversed (self.graph.nodes_topologically_sorted()):
+     gate.backguard() # little piece of backp`rop(chain rule applied)
  return input_gradients
